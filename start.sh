f214d924def89959a5c8a9f6707598b812e050e0
dhanush2bot <149936819+dhanush2bot@users.noreply.github.com>
2023-12-06 16:38:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,16 @@
-if [ -z $UPSTREAM_REPO ]
-then
+#!/bin/bash
+
+if [ -z $UPSTREAM_REPO ]; then
   echo "Cloning main Repository"
   git clone https://github.com/dhanush2bot/testing-
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /testing-
 fi
+
 cd /testing-
-pip3 install -U -r requirements.txt
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
+
 echo "Starting Bot...."
 python3 bot.py
