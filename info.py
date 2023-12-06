abefe560d5cb03554bdc4e75178299e0e455b73f
dhanush2bot <149936819+dhanush2bot@users.noreply.github.com>
2023-12-06 20:10:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "INDEX")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shareus.io')
 SHORTLINK_API = environ.get('SHORTLINK_API', '0ft2s2mKCHOaLUKDZCcnDXCt8O63')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
